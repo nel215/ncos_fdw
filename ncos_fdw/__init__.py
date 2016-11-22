d40943a1c1f5cb8fb4901df1507ef035e99894cf
nel215 <otomo.yuhei@gmail.com>
2016-11-23 01:08:51 +0900

    Add FDW for NCOS

---
+++ ncos_fdw/__init__.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+from multicorn import ForeignDataWrapper
+from multicorn.utils import log_to_postgres
+import logging
+import json
+import boto3
+from botocore.client import Config
+
+
+class NCOSForeignDataWrapper(ForeignDataWrapper):
+
+    def __init__(self, options, columns):
+        super(NCOSForeignDataWrapper, self).__init__(options, columns)
+        self.columns = columns
+        self.endpoint = options['endpoint']
+        self.access_key = options['access_key']
+        self.secret_key = options['secret_key']
+        self.bucket = options['bucket']
+        self.prefix = options['prefix']
+        log_to_postgres(message=str(options), level=logging.WARNING)
+
+    def execute(self, quals, columns):
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key,
+            endpoint_url=self.endpoint,
+            config=Config(signature_version='s3'),
+        )
+        log_to_postgres(message=str(quals), level=logging.WARNING)
+        log_to_postgres(message=str(columns), level=logging.WARNING)
+        obj_list = s3.list_objects(Bucket=self.bucket, Prefix=self.prefix)
+        for c in obj_list['Contents']:
+            res = s3.get_object(Bucket=self.bucket, Key=c['Key'])
+            for row in res['Body'].read().split('\n'):
+                record = json.loads(row.split('\t')[2])
+                yield record
